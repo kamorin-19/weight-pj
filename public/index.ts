2531ef017b0e5eba58aa55acfeef8a69afadc680
kamorin-19 <159443759+kamorin-19@users.noreply.github.com>
2025-02-18 20:22:03 +0900

    不要なコンソールを削除

--- public/index.ts
+++ public/index.ts
@@ -1,24 +1,19 @@
 import axios from "axios";
 
 const handleLoginClick = async (): Promise<void> => {
-  console.log("★");
+
   const usernameInput = document.getElementById('username') as HTMLInputElement | null;
   const passwordInput = document.getElementById('password') as HTMLInputElement | null;
 
   if (!usernameInput || !passwordInput) return;
-  console.log("★★");
 
   const username = usernameInput.value;
   const password = passwordInput.value;
 
   try {
-    console.log("★★★");
     const response = await axios.post('http://localhost/login', { username, password });
-    console.log(1);
     console.log('Response:', response.data);
   } catch (error) {
-    console.log("★★★★");
-    console.log(2);
     console.error('Error:', error);
   }
 };
